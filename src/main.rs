cc9958935351c6b2b4dded1a4061d01ed70099e9
Salvador Guzman <guzmansalv@gmail.com>
2019-05-21 15:21:27 -0700

    dud

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+extern crate rafy;
+use rafy::Rafy;
+
 fn main() {
-    println!("Hello, world!");
+    let content = Rafy::new("https://www.youtube.com/watch?v=DjMkfARvGE8").unwrap();
+    println!("{}", content.videoid);
+    println!("{}", content.title);
+    println!("{}", content.rating);
+    println!("{}", content.viewcount);
 }
